a15487da5abec535b98e09427a05db8e83b887c3
Strassboom <cat.strassburg@gmail.com>
2022-10-26 00:42:45 -0400

    Completed authentication processes and script runability tests!!!

--- dbOperations.py
+++ dbOperations.py
@@ -1,65 +1,86 @@
+from getpass import getpass
 import sqlite3
 from datetime import datetime
 import base64
 
 # Initializes connection and tables of db, or creates db if it doesn't exist
 def initDB(dbName):
-    sqliteConnection = createConnection(dbName)
-    cursor = sqliteConnection.cursor()
+    conn = createConnection(dbName)
     print("DB Created")
-    initTables(cursor)
-    return cursor
+    initTables(conn)
+    authPassword = "bubblegum"
+    if findDrive(conn,'ADMIN') is False:
+        dtnow = datetime.now().strftime("%Y-%m-%d %I:%M:%S")
+        conn.execute(f""" INSERT INTO Authkeys (Name,key,registeredDateTime) VALUES ('ADMIN','{authPassword}','{dtnow}');""")
+    return conn
 
 # Creates connection to sql database of name dbName
 def createConnection(dbName):
     return sqlite3.connect(f"{dbName}")
-    print("Connection Created")
 
 # Creates tables if they do not exist
-def initTables(cursor):
-    tables = [["Events",""" CREATE TABLE Events (
+def initTables(conn):
+    tables = [["Events",""" CREATE TABLE IF NOT EXISTS Events (
             Name VARCHAR(255) NOT NULL,
             Type VARCHAR(255) NOT NULL,
             eventDateTime DATETIME NOT NULL
         ); """],
-            ["Authkeys",""" CREATE TABLE Authkeys (
-                Name VARCHAR(255) NOT NULL,
+            ["Authkeys",""" CREATE TABLE IF NOT EXISTS Authkeys (
+                Name VARCHAR(255) UNIQUE NOT NULL,
                 key VARCHAR(255) NOT NULL,
                 registeredDateTime DATETIME NOT NULL
             ); """]]
     for i in tables:
         try:
-            cursor.execute(i[1])
+            conn.execute(i[1])
             print(f"{i[0]} table created!")
         except:
             continue
     print("All Tables created!")
 
 # Searches Authkeys database for currently plugged in drive
-def findDrive(cursor, name):
-    drive = cursor.execute(f""" SELECT * from Authkeys WHERE Name = '{name}';""").fetchone()
-    print(drive)
+def findDrive(conn, name):
+    drive = conn.execute(f""" SELECT * from Authkeys WHERE Name = '{name}';""").fetchone()
     if drive:
         return True
     return False
 
 # Authenticate Drive
-def authenticateDrive(cursor, name, key):
-    auth = cursor.execute(f""" SELECT * from Authkeys WHERE Name = '{name}' and key = '{key}'""").fetchone()
+def authenticateDrive(conn, name, key):
+    auth = conn.execute(f""" SELECT * from Authkeys WHERE Name = '{name}' and key = '{key}'""").fetchone()
     if auth:
         return True
     else:
         return False
 
-# Adds Drive to AuthKeys table if password correct
-def insertDrive(cursor, name, keyDT = datetime.now().strftime("%Y-%m-%d %I:%M:%S")):
+def createAuthKey(name, keyDT = datetime.now().strftime("%Y-%m-%d %I:%M:%S")):
     keyUnEncoded = name + "_" + keyDT
     encodedKey = keyUnEncoded.encode("ascii")
     base64KeyBytes = base64.b64encode(encodedKey)
-    base64KeyString = base64KeyBytes.decode("ascii")
-    key = base64KeyString
-    cursor.execute(f""" INSERT INTO Authkeys (Name,key,registeredDateTime) VALUES ('{name}','{key}','{keyDT}');""")
+    return base64KeyBytes.decode("ascii")
+
+def getAuthKey(conn, name):
+    return conn.execute(f""" SELECT key FROM Authkeys WHERE Name = '{name}';""").fetchone()[0]
+
+# Adds Drive to AuthKeys table if password correct
+def insertDrive(conn, name, keyDT = datetime.now().strftime("%Y-%m-%d %I:%M:%S")):
+    key = createAuthKey(name, keyDT)
+    conn.execute(f""" INSERT INTO Authkeys (Name,key,registeredDateTime) VALUES ('{name}','{key}','{keyDT}');""")
     return key
 
-dbName = "sql.db"
-initDB(dbName)
+def getAdminPassword(conn):
+    authPassword = conn.execute(f""" SELECT key from Authkeys WHERE Name = 'ADMIN'""").fetchone()[0]
+    return authPassword
+
+def adminAuth(conn,password):
+    findDrive(conn,'ADMIN')
+    return getAdminPassword(conn) == password
+
+def getAllAuthKeys(conn):
+    return conn.execute(f""" SELECT * FROM Authkeys;""").fetchall()
+
+# dbName = "sql.db"
+# conn = initDB(dbName)
+# print(conn.execute(f""" SELECT * FROM Authkeys;""").fetchall())
+#print(findDrive(conn,"D:"))
+#adminAuth(initDB(dbName),"bubblegum")
--- inputChecker.py
+++ inputChecker.py
@@ -32,7 +32,7 @@ def listWindowsDrives() -> List[Drive]:
         text=True,
         stdout=subprocess.PIPE
     )
-    print(proc.stdout)
+    #print(proc.stdout)
     if proc.returncode != 0 or not proc.stdout.strip():
         print('Failed to enumerate drives')
         return []
@@ -58,21 +58,56 @@ def windowsDriveEject(driveName):
     os.system(f'powershell (New-Object -comObject Shell.Application).Namespace(17).ParseName("""{driveName}""").InvokeVerb("""Eject""")')
 
 def driveCheckerSetup(dbName):
-    cursor = dbOperations.initDB(dbName)
+    cur_dir = os.path.abspath(".")
+    conn = dbOperations.initDB(dbName)
     if platform == 'win32':
         driveEject = windowsDriveEject
         listDrives = listWindowsDrives
         drives = listDrives()
         for drive in drives:
             if drive.drive_type == 'Removable Disk':
-                if dbOperations.findDrive(drive.letter):
-                    if dbOperations.authenticateDrive(cursor,drive.letter):
-                        continue
+                if dbOperations.findDrive(conn,drive.letter):
+                    if os.path.exists(f"{drive.letter}\\driveAuth.txt") is False:
+                        if dbOperations.adminAuth(conn,input("Auth File does not exist. Please enter the password to create your auth: ")):
+                            with open(f"{drive.letter}\\driveAuth.txt","w+") as driveAuth:
+                                driveAuth.write(dbOperations.getAuthKey(conn,drive.letter))
+                            print("Password correct! Auth Created! Re-insert flash drive to run again")
+                        else:
+                            print("Password invalid. Re-insert flash drive and try again")
+                    else:
+                        with open(f"{drive.letter}\\driveAuth.txt","r") as driveAuth:
+                            if dbOperations.authenticateDrive(conn,drive.letter,driveAuth.readlines()[0]):
+                                print("Drive authenticated! Beginning drive processes:")
+                                os.chdir(f"{drive.letter}\\")
+                                with open(f"{drive.letter}\\processes.txt","r") as executor:
+                                    for line in executor.readlines():
+                                        os.system(line)
+                                print("All processes successfully run!")
+                            elif dbOperations.adminAuth(conn,input("Auth is invalid. Please enter the password to recover your auth: ")):
+                                with open(f"{drive.letter}\\driveAuth.txt","w+") as driveAuth:
+                                    driveAuth.write(dbOperations.getAuthKey(conn,drive.letter))
+                                print("Password correct! Re-insert flash drive to run again")
+                            else:
+                                print("Password invalid. Re-insert flash drive and try again")
+                else:
+                    if dbOperations.adminAuth(conn,input("Drive not found. Please enter the password to create your auth: ")):
+                        dbOperations.insertDrive(conn,drive.letter)
+                        print("Password correct! Drive and auth added to database!")
+                        with open(f"{drive.letter}\\driveAuth.txt","w+") as driveAuth:
+                            driveAuth.write(dbOperations.createAuthKey(drive.letter))
+                        print("Auth added to Drive! Re-insert flash drive to run again")
+                    else:
+                        print("Password invalid. Re-insert flash drive and try again")
                 driveEject(drive.letter)
+                print(f"Drive {drive.letter} successfully ejected!")
+        conn.commit()
+        drives = listDrives()
         return drives
         
+#def driveCheckerLoop(conn):
 
 if __name__ == '__main__':
     disk = "D:"
     dbName = "sql.db"
-    print(driveCheckerSetup(dbName))
+    drives = driveCheckerSetup(dbName)
+    print(drives)
